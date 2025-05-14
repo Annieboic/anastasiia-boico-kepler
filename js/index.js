6cd83920f36fa7971f951f9df1b8917c2e5cf88d
Anastasiia Boico <167498327+Annieboic@users.noreply.github.com>
2025-05-14 13:59:03 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -10,14 +10,14 @@ const thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
 
 footer.classList.add("footer_element");
-
+const copyrightSymb = "\u00A9";
 
 const copyright = document.createElement("p");
-copyright.innerHTML =  `${nameFooter} © ${thisYear}`;
+copyright.innerHTML =  `${nameFooter} ${copyrightSymb} ${thisYear}`;
 
 copyright.classList.add("copyright")
 
-footer.appendChild(copyright);
+footer.appendChild(copyright); 
 
 //styling Footer
 
@@ -40,5 +40,53 @@ for (let i = 0; i < skills.length; i++){
     skill.classList.add("skill");
     skillsList.appendChild(skill)
 }
+
+//Message Form
+
+
+
+const messageForm = document.querySelector('form');
+messageForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const nameText = event.target.usersName.value;
+    const emailText = event.target.usersEmail.value;
+    const messageText = event.target.usersMessage.value;
+
+    console.log("Name:", nameText);
+    console.log("Email:", emailText);
+    console.log("Message:", messageText);
+
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+    
+    newMessage.innerHTML = `<a href="mailto:${emailText}">${nameText}</a> <span>${messageText}</span>`;
+
+    messageList.appendChild(newMessage);
+
+    
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute("type", "button");
+    removeButton.addEventListener("click", function(event){
+        const entry = newMessage.parentNode; //ul element chosen
+        
+        newMessage.remove(); //remove list element after clicking
+
+    })
+
+    newMessage.appendChild(removeButton);
+    
+    
+    messageForm.reset();
+
+
+  });
+
+
+  // (Optional) Hide the #messages section, including the Messages header, when the list is empty
+ //(Optional) Create an "edit" button for each message entry that allows the user to input a new/modified message
 
 
